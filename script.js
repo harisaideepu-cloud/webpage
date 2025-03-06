bc9c9d47d34fa921c61a12ea2d1d462bfebfd621
harisaideepu-cloud <harisaideepu1@gmail.com>
2025-03-06 12:16:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Smooth scroll to main content when the button is clicked
+function scrollToContent() {
+    document.getElementById("main-content").scrollIntoView({
+        behavior: 'smooth'
+    });
+}
